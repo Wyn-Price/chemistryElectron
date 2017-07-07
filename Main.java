44678e209c72ea6ca0d727d2c919a303c26e0f97
Wyn Price <wynprice999@gmail.com>
2017-07-07 14:52:51 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,503 @@
+package com.wynprice.chemistry;
+
+import java.awt.Dimension;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		createGui();
+		IsKeyPressed.main();
+	}
+	
+	private static JTextField textInput;
+	private static JLabel outputText;
+	private static JFrame frame;
+	private static String fText = "";
+	private static JButton button;
+	
+	
+	private static void createGui()
+	{
+		frame = new JFrame("Enter Atomic Number");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		textInput = new JTextField();
+		outputText = new JLabel("", SwingConstants.CENTER);
+		String spacing = "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ";
+		button = new JButton("<html>" + spacing + "Click to submit<br>Shift + Click for infomation per shell</html>");
+		JPanel panel = new JPanel();
+		
+		frame.add(textInput);
+		frame.add(button);
+		frame.pack();
+		frame.add(outputText);
+		frame.setVisible(true);
+		
+		panel.add(textInput);
+		frame.add(panel);
+
+		frame.setSize(new Dimension(900, 600));
+		textInput.setPreferredSize(new Dimension(100, 30));
+		
+		outputText.setBackground(UIManager.getColor ( "Panel.background" ));
+		
+		
+		button.addActionListener(new ActionListener() { 
+		    public void actionPerformed(ActionEvent e) { 
+		    	calc((e.getModifiers() & InputEvent.SHIFT_MASK) != 0);
+		    } 
+		});
+		
+		frame.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentHidden(ComponentEvent e) {
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent e) {				
+			}
+
+			@Override
+			public void componentResized(ComponentEvent e) {
+				outputText.setSize(new Dimension(frame.getSize().width - 50, frame.getSize().height));
+				outputText.setLocation(25, 0);
+				button.setLocation(frame.getSize().width / 2 - 125, frame.getSize().height - 90);
+				resizeTest();
+			}
+
+			@Override
+			public void componentShown(ComponentEvent e) {				
+			}
+		});
+
+	}
+	
+	private static ArrayList<String> elementNames = new ArrayList<String>();
+	private static String[] defaultElementNames = {"hydrogen","helium","lithium","beryllium","boron","carbon",
+		"nitrogen","oxygen","fluorine","neon","sodium","magnesium","aluminum","silicon",
+		"phosphorus","sulfur","chlorine","argon","potassium","calcium","scandium","titanium",
+		"vanadium","chromium","manganese","iron","cobalt","nickel","copper","zinc","gallium",
+		"germanium","arsenic","selenium","bromine","krypton","rubidium","strontium","yttrium",
+		"zirconium","niobium","molybdenum","technetium","ruthenium","rhodium","palladium","silver",
+		"cadmium","indium","tin","antimony","tellurium","iodine","xenon","cesium","barium","lanthanum",
+		"cerium","praseodymium","neodymium","promethium","samarium","europium","gadolinium","terbium",
+		"dysprosium","holmium","erbium","thulium","ytterbium","lutetium","hafnium","tantalum","tungsten",
+		"rhenium","osmium","iridium","platinum","gold","mercury","thallium","lead","bismuth","polonium",
+		"astatine","radon","francium","radium","actinium","thorium","protactinium","uranium","neptunium",
+		"plutonium","americium","curium","berkelium","californium","einsteinium","fermium","mendelevium",
+		"nobelium","lawrencium","rutherfordium","dubnium","seaborgium","bohrium","hassium","meitnerium",
+		"darmstadtium","roentgenium","ununbium","ununtrium","ununquadium","ununpentium","ununhexium",
+		"ununseptium","ununoctium"};
+	
+	
+	private static ArrayList<String> elementSymbol = new ArrayList<String>();
+	private static String[] defaultElementSymbols = {"h","he","li","be","b","c","n","o","f","ne","na","mg","al","si","p","s",
+		"cl","ar","k","ca","sc","ti","v","cr","mn","fe","co","ni","cu","zn","ga","ge","as","se","br","kr",
+		"rb","sr","y","zr","nb","mo","tc","ru","rh","pd","ag","cd","in","sn","sb","te","i","xe","cs","ba",
+		"la","ce","pr","nd","pm","sm","eu","gd","tb","dy","ho","er","tm","yb","lu","hf","ta","w","re","os",
+		"ir","pt","au","hg","tl","pb","bi","po","at","rn","fr","ra","ac","th","pa","u","np","pu","am","cm",
+		"bk","cf","es","fm","md","no","lr","rf","db","sg","bh","hs","mt","ds","rg","uub","uut","uuq","uup",
+		"uuh","uus","uuo"};
+	
+	public static void calc(boolean isShift)
+	{
+		Collections.addAll(elementNames, defaultElementNames);
+		Collections.addAll(elementSymbol, defaultElementSymbols);
+		int electrons = 0;
+		boolean flag = true;
+		if(elementNames.contains(textInput.getText().toLowerCase()))
+		{
+			flag = false;
+			electrons = elementNames.indexOf(textInput.getText().toLowerCase());
+		}
+		if(elementSymbol.contains(textInput.getText().toLowerCase()))
+		{
+			flag = false;
+			electrons = elementSymbol.indexOf(textInput.getText().toLowerCase());
+		}
+		if(flag)
+		{
+			try
+			{
+				electrons = Integer.parseInt(textInput.getText());
+			}
+			catch (NumberFormatException e) {
+				outputText.setText("<html>Error: '" + textInput.getText() + "' is not a number</html>");
+				return;
+			}
+			if(electrons < 0 )
+			{
+				outputText.setText("<html>Error: '" + textInput.getText() + "' is not a number</html>");
+				return;
+			}
+		}
+		
+		fText = isShift ? new Atom(electrons).getCompStruc() : new Atom(electrons).getNormStruc();
+		resizeTest();
+	}
+	
+	private static void resizeTest()
+	{
+		String st = "";
+		int num = fText.startsWith("s") ? 3 : 25;
+		long n = Math.round(frame.getSize().getWidth() / num);
+		String[] splitString =  fText.split(", ");
+		if(splitString.length > n)
+			for(int i = 0; i < splitString.length; i++)
+				if(i == splitString.length-1) st += splitString[i];
+				else if(i % n != 0) st += splitString[i] + ", ";
+				else st += splitString[i] + ",<br>";
+		else st = fText;
+		outputText.setText("<html>" + st + "</html>");
+	}
+}
+
+class Atom 
+{
+	private ArrayList<Shell> shells = new ArrayList<Shell>();
+	private int e;
+	public Atom(int totalElectrons)
+	{
+		shellNoticableChange.clear();
+		shellNoticableChange.add(18);
+		k = 0;
+		ArrayList<Integer> j = new ArrayList<Integer>();
+		int[] p = {1,3,11,19,37,55,87,119};
+		for (int l : p)
+			j.add(l);
+		boolean extendListDone = false;
+		int s = 14;
+		int o = 0;
+		while(!extendListDone)
+			if(totalElectrons >= j.get(j.size()-1))
+			{
+				if(o % 2 == 1)
+					s += 4;
+				j.add(s + j.get(j.size()-1));
+				o++;
+			}
+			else
+				extendListDone = true;
+		for(int i = 0; i < j.size(); i ++)
+		{
+			if(totalElectrons >= j.get(i))
+				shells.add(new Shell(i));
+		}	
+		e = totalElectrons;
+		for(int i = 0; i < shells.size(); i ++)
+		{
+			simpleAdd(i);
+			if(i+1==shells.size() || i < 2 || totalElectrons < 21)
+				continue;
+			addS(i+1,2);
+			Shell prev = shells.get(i - 1);
+			Shell next = shells.get(i + 1);
+			if(next.s == 2)
+			{
+				if(prev.hasF())
+				{
+					addD(i, 1);
+					addF(i-1, 14);
+					if(shells.get(i - 2).getMax() > 14)
+						createCustomShells(shells.get(i - 2));
+
+						
+					addD(i, 13);
+				}
+				else
+					addD(i, 10);
+			}
+		}
+		
+		ArrayList<Shell> finishShell = new ArrayList<Shell>();
+		for(Shell shell : shells)
+			if(shell.getAdded() != 0)
+				finishShell.add(shell);
+		shells = finishShell;
+		
+	}
+	
+	private void createCustomShells(Shell shell) 
+	{
+		int k = ((shell.getMax() - 2) / 4) - 4;
+		for(int j = 0; shell.customSubs.size() < k; j++)
+			shell.createNewSub((j * 4) + 18);
+		
+		addToCustomShell(shell, shell.getMax());
+	}
+	
+	private ArrayList<Integer> shellNoticableChange = new ArrayList<Integer>();
+	int k = 0;
+	
+	private void addToCustomShell(Shell shell, int amount)
+	{
+		k++;
+		if(shellNoticableChange.get(shellNoticableChange.size()-1) < amount)
+			shellNoticableChange.add(amount);
+		for(int i = 0; i < k; i++)
+		{
+			if(shells.get(shell.getPosition() - i).getMax() > shellNoticableChange.get(i))
+			{
+				int n;
+				if(e <= shellNoticableChange.get(i))
+					n = e;
+				else
+					n = shellNoticableChange.get(i);
+				e -=shells.get(shell.getPosition() - i).addCustomSub(i, n);
+			}
+				
+		}	
+	}
+
+	public String getNormStruc()
+	{
+		String st = "";
+		for(Shell s : shells)
+			st+= s.addAll() + ", ";
+		return st.substring(0, st.length() - 2);
+	}
+	
+	public String getCompStruc()
+	{
+		String st = "";
+		for(Shell s : shells)
+		{
+			st+= "<u>Shell " + s.getPosition() + "</u>: " + s.all() + "<br>";
+
+		}
+		return st;
+	}
+	
+	private void simpleAdd(int shellPosition)
+	{
+		addS(shellPosition,2);
+		addP(shellPosition,6);
+	}
+	
+	private void addS(int shellPosition, int addative)
+	{
+		int num = e >= addative? addative : e;
+		e += shells.get(shellPosition).addS(num);
+		e -= num;		
+	}
+	
+	private void addP(int shellPosition, int addative)
+	{
+		if(shellPosition == 0)
+			return;
+		int num = e >= addative? addative : e;
+		e += shells.get(shellPosition).addP(num);
+		e -= num;		
+	}
+	
+	private void addD(int shellPosition, int addative)
+	{
+		int num = e >= addative? addative : e;
+		e += shells.get(shellPosition).addD(num);
+		e -= num;	
+	}
+	
+	private void addF(int shellPosition, int addative)
+	{
+		int num = e >= addative? addative : e;
+		e += shells.get(shellPosition).addF(num);
+		e -= num;	
+	}
+}
+class Shell 
+{
+	private final int shellPosition;
+	public int s=0, p=0, d=0, f=0;
+	public static ArrayList<Shell> allShells = new ArrayList<Shell>();
+	public ArrayList<Integer> customSubs = new ArrayList<Integer>();
+	public ArrayList<Integer> customSubHolding = new ArrayList<Integer>();
+
+	
+	public int getMax()
+	{
+		return 6 + (shellPosition * 4);
+	}
+	
+	public int getAdded()
+	{
+		int i = s + p + d + f;
+		for(int j : customSubHolding)
+			i += j;
+		return i;
+	}
+	
+	public Shell(int shellPosition)
+	{
+		this.shellPosition = shellPosition;
+		allShells.add(this);
+		
+	}
+	
+	public int getPosition()
+	{
+		return shellPosition;
+	}
+	
+	public String all()
+	{
+		ArrayList<String> fAl = new ArrayList<String>();
+		ArrayList<String> alphabet = new ArrayList<String>();
+		Collections.addAll(fAl, "abcdefghijklmnopqrstuvwxyz".split(""));
+		Collections.addAll(alphabet, "abceghijklmnoqrtuvwxyz".split(""));
+		String preffix = "";
+		String suffix = "";
+		int o = 0;
+		char[] m = "spdf".toCharArray();
+		int[]sf = {s,p,d,f};
+		for(int i = 0; i < sf.length; i++)
+		{
+			if(sf[i] == 0)
+				continue;
+			if(i==0)
+				suffix += "s: " + sf[0];
+			else
+				suffix += ",&nbsp " + m[i] + ": " + sf[i];
+		}
+		for(int i = 0; i < customSubHolding.size(); i ++)
+		{
+			if(customSubHolding.get(i) == 0)
+				continue;
+			try
+			{
+				preffix += ",&nbsp "  + alphabet.get(i) + ": " + customSubHolding.get(i);
+			}
+			catch (IndexOutOfBoundsException e) {
+				o++;
+				ArrayList<String> w = new ArrayList<String>();
+				for(int l = 0; l < fAl.size(); l++)
+					w.add(String.valueOf(fAl.get(l)) + o);
+				for(String s : w)
+					alphabet.add(s);
+				preffix += ",&nbsp "  + alphabet.get(i) + ": " + customSubHolding.get(i);
+			}
+		}
+			
+		
+		return suffix + preffix;
+	}
+	
+	public String addAll()
+	{
+		return String.valueOf(getAdded());
+	}
+	
+	public void createNewSub(int initialLimit)
+	{
+		customSubs.add(initialLimit);
+	}
+	
+	public int addCustomSub(int positionNumber, int amount)
+	{
+		while(customSubHolding.size() < positionNumber + 1)
+			customSubHolding.add(0);
+		
+		customSubHolding.set(positionNumber, amount);
+		return amount;
+	}
+	
+	public int addS(int s)
+	{
+		this.s += s;
+		if(this.s > 2)
+		{
+			int r = this.s - 2;
+			this.s = 2;
+			return r;
+		}
+		return 0;
+	}
+	
+	public int addP(int p)
+	{
+		this.p += p;
+		if(this.p > 6)
+		{
+			int r = this.p - 6;
+			this.p = 6;
+			return r;
+		}
+		return 0;
+	}
+
+	public int addD(int d)
+	{
+		this.d += d;
+		if(this.d > 10)
+		{
+			int r = this.d - 10;
+			this.d = 10;
+			return r;
+		}
+		return 0;
+	}
+	
+	public int addF(int f)
+	{
+		this.f += f;
+		if(this.f > 14)
+		{
+			int r = this.f - 14;
+			this.f = 14;
+			return r;
+		}
+		return 0;
+	}
+	
+	public Boolean hasF()
+	{
+		return shellPosition > 2;
+	}
+}
+
+class IsKeyPressed {
+    private static volatile boolean Shift = false;
+
+    public static void main() {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent ke) {
+                synchronized (IsKeyPressed.class) {
+                    switch (ke.getID()) {
+                    case KeyEvent.KEY_PRESSED:
+                    	if(ke.getKeyCode() == KeyEvent.VK_SHIFT)
+                    		Shift = true;
+                        if (ke.getKeyCode() == KeyEvent.VK_ENTER) 
+                        	Main.calc(Shift);
+                        break;
+
+                    case KeyEvent.KEY_RELEASED:
+                        if (ke.getKeyCode() == KeyEvent.VK_SHIFT) {
+                        	Shift = false;
+                        }
+                        break;
+                    }
+                    return false;
+                }
+            }
+        });
+    }
+}
